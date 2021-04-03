a7d094f1033e9e9012162d1d601d365259e9339a
Yinan Qiu <yinanq@users.noreply.github.com>
2021-04-03 00:06:50 -0700

    initial project creation

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Yinan Qiu on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
